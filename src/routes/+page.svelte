06fe6880c9fdcb08cf682e59605e38a68306f9b8
Yikuan Sun <yikuansun2015@gmail.com>
2025-07-19 13:18:11 -0400

    Add controls
    Very good

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,54 @@
 <script lang="ts">
     import ResumeShell from "$lib/components/ResumeShell.svelte";
+
+    let resumeData: { [key: string]: any } = {
+        name: "Yikuan",
+        email: "yikuan@example.com",
+    };
 </script>
 
-<div style:width="800px" style:height="600px" style:background-color="black"
-    style:padding="20px">
-    <ResumeShell />
+<div id="editPanel">
+    <label>
+        Name
+        <input type="text" bind:value={resumeData.name} />
+    </label>
+    <label>
+        Email
+        <input type="email" bind:value={resumeData.email} />
+    </label>
 </div>
+
+<div id="previewPanel">
+    <ResumeShell data={resumeData} />
+</div>
+
+<style>
+    #editPanel {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 50%;
+        height: 100vh;
+        background-color: #242424;
+        box-sizing: border-box;
+        padding: 36px;
+        font-size: 28px;
+        color: whitesmoke;
+    }
+
+    #editPanel label {
+        display: block;
+        margin-bottom: 12px;
+    }
+
+    #previewPanel {
+        position: fixed;
+        top: 0;
+        right: 0;
+        width: 50%;
+        height: 100vh;
+        background-color: #121212;
+        box-sizing: border-box;
+        padding: 20px;
+    }
+</style>

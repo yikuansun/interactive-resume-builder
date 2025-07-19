b563533b994028237c26462dddf5efddce3cff78
Yikuan Sun <yikuansun2015@gmail.com>
2025-07-19 13:06:26 -0400

    Create printable page
    Cool

---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import ResumeShell from "$lib/components/ResumeShell.svelte";
+</script>
+
+<div style:width="100vw" style:height="100vh" style:background-color="black"
+    style:position="fixed" style:top="0" style:left="0">
+    <ResumeShell name="Yikuan" />
+</div>
+
+<style>
+    @media print {
+        @page {
+            margin: 0;
+        }
+    }
+</style>
